12da3ec8bc9e06bc2faaf93a5922c26effaf74bf
kefvin <kevinappenrodt14@gmail.com>
2015-09-28 15:17:09 +0200

    Añadido programa que pide 5 numeros y da el mas pequeño

---
+++ Subversion.java
@@ -0,0 +1,20 @@
+package Subversion;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Subversion {
+	public static void main (String[]args){
+		Scanner lector = new Scanner(System.in);
+		
+		int[] array=new int[5];
+		
+		for(int i=0;i<array.length;i++){
+			System.out.println("Entra un numero entero: ");
+			array[i]=lector.nextInt();
+			lector.nextLine();
+		}
+		Arrays.sort(array);
+		System.out.println(array[0]);
+	}
+}
